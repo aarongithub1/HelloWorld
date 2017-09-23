5c3aa4bcd319492662d8879964c706a0e0709645
Aaron Ipp <31551830+aarongithub1@users.noreply.github.com>
2017-09-23 01:54:21 -0600

    cause merger conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!!!");
-        System.out.println("Programming is great!");
+        System.out.println("Changing items in github is a bad idea");
     }
 }
